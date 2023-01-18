b6c1da981040917ce5580e5cd298db21fa98fc00
Cyandied <awesome.pie753@gmail.com>
2023-01-18 20:18:42 +0100

    rtrgrt

---
+++ Start.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+st.set_page_config(
+    page_title="Start"
+)
+
+st.write(
+    """
+    # Welcome to the reciept manager
+    ## Please select your service on the menu to the left.
+    """
+)
