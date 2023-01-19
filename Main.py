4fe2cbdb39db4081f6ffbdbd8f53d525b8df7348
Cyandied <awesome.pie753@gmail.com>
2023-01-19 22:32:09 +0100

    Added error messages... and other functionality

--- Main.py
+++ Main.py
@@ -3,17 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 import func
 import json
+import time
 
 contributers = []
-relavantFile = "none"
+reciept = None
+
+i = 0
+item_index = 0
+item_from_Recipt = None
+subtotal = 0
+showContri = None
 
 sg.theme('DarkPurple3')
 
 # All the stuff inside your window.
-layout = [  [sg.Text('Welcome to the receipt calculator!')],
-            [sg.Text('Begin new receipt:'), sg.Button("New")],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Exit')] ]
+layout = [
+    [sg.Text('Welcome to the receipt calculator!')],
+    [sg.Text('Begin new receipt:'), sg.Button("New")],
+    [sg.Text('Enter something on Row 2'), sg.InputText()],
+    [sg.Button('Ok'), sg.Button('Exit')]
+]
+
+def ItemList(rcpt = reciept):
+
+    if rcpt == None:
+        return sg.Text("No items to display")
+    elif len(rcpt.item) == 0: 
+        return sg.Text("No items to display")
+    else:
+        return sg.Text("Items:")
+
+itemList = []
+layout2 = [
+    [sg.Text('Please input a date:'), sg.CalendarButton("Calendar", target = "date"), sg.In(key = "date")],
+    [sg.Text("Please input a store:"), sg.InputText(key = "store")],
+    [sg.Text("Please input who payed:"), sg.InputText(key = "paidBy")],
+    [sg.Text("Write name of contributer:"), sg.InputText(key = "contri"), sg.Button("Add")],
+    [sg.Text("List of contributers not including who paid: "), sg.Text(key = "-OUTPUT-")],
+    [sg.Button("Create reciept"), sg.Text(key = "-OUTPUT2-")],
+    [sg.Button("Add item")],
+    [ItemList(), sg.Listbox(values = itemList, size= (40,6), key = "ItemList"), sg.Button("Change item")],
+    [sg.Text("Contibuters are to pay:"), sg.Text("shit",key = "ToPay"), sg.Text("Subtotal is:"), sg.Text(subtotal,key = "subtotal")],
+    [sg.Button('Exit'), sg.Button("Finalize reciept")]
+]
 
 # Create the Window
 win1 = sg.Window('Start', layout)
@@ -28,69 +60,159 @@ while True:
 
     if not win2_active and ev1 == 'New':
         win2_active = True
-        layout2 = [[sg.Text('Please input a date:'), sg.CalendarButton("Calendar", target = "date"), sg.In(key = "date")],
-                    [sg.Text("Please input a store:"), sg.InputText(key = "store")],
-                    [sg.Text("Please input who payed:"), sg.InputText(key = "payer")],
-                    [sg.Text("Write name of contributer:"), sg.InputText(key = "contri"), sg.Button("Add")],
-                    [sg.Text("List of contributers not including payer: "), sg.Text(key = "-OUTPUT-")],
-                    [sg.Button("Create receipt"), sg.Text(key = "-OUTPUT2-")],
-                    [sg.Button("Add item")],
-                   [sg.Button('Exit')]]
-
         win2 = sg.Window('New receipt', layout2)
 
     if win2_active:
         ev2, vals2 = win2.read(timeout=100)
+
         if ev2 == sg.WIN_CLOSED or ev2 == 'Exit':
             win2_active  = False
             win2.close()
+
         if ev2 == "Add":
             contributers.append(vals2["contri"])
             win2["-OUTPUT-"].update(contributers)
             win2['contri'].Update('')
 
-        if ev2 == "Create receipt":
-            relavantFile = func.CreateFile(vals2["date"][:10],vals2["store"], vals2["payer"], contributers)
-            win2["-OUTPUT2-"].update(f"Receipt sucessfully created, relavant file is now: {relavantFile[8:]}")
+        if ev2 == "Create reciept":
+            showContri = [vals2["paidBy"], *contributers]
+            reciept = func.Basics(vals2["date"][:10],vals2["store"], vals2["paidBy"], showContri)
+            win2["-OUTPUT2-"].update(f"Receipt sucessfully created.")
+        
+        if ev2 == "Finalize reciept":
+            item_string = ""
+            for item in reciept.items:
+                item_string =  f'{item_string}\n\tName: {item.name} \n\tPrice: {item.price} \n\tDiscount: {item.discount} \n\tContributers: {item.contributers} \n'
+            answer = sg.popup_scrolled(f"Is the following information correct?: \nDate: {reciept.date} \nStore: {reciept.store} \nPaid by: {reciept.paidBy} \nItems: {item_string}",yes_no= True,title = "Check if into is correct")
+            if answer == "Yes":
+                sg.popup("Reciept pushed to external server.", button_type = sg.POPUP_BUTTONS_OK)
+                print("Pushing data")
+                win2_active  = False
+                win2.close()
+            if answer == "No":
+                print("Not pushing data")
         
         if ev2 == "Add item" and not winItem_active:
             winItem_active = True
-            f = open(relavantFile)
-            dict = json.loads(f.read())
-            showContri = [dict["payer"], *dict["contributers"]]
-            newContri = []
-            checkboxes = [sg.Checkbox(x) for x in showContri]
             col = [
                 [sg.Text("Welcome to the item creator!")],
                 [sg.Text("Name of item"), sg.Input(key = "name")],
                 [sg.Text("Price of item"), sg.Input(key = "price")],
-                [sg.Text("Discount on item"), sg.Input(key = "discount")],
+                [sg.Text("Discount on item"), sg.Input(key = "discount")]
             ]
-            f.close()
+            checkboxes = [sg.Checkbox(x) for x in showContri]
             layoutItem = [
                 [sg.Column(col)],
-                [checkboxes, sg.Button("Add"), sg.Text(key = "-OUTPUT3-")],
-                [sg.Button("Finish"), sg.Button("Exit")]
+                [checkboxes],
+                [sg.Button("Finish"), sg.Button("Cancel")]
             ]
-
             winItem = sg.Window("Item creator", layoutItem)
         
+        if ev2 == "Change item" and not winItem_active:
+            if len(vals2["ItemList"]) == 0:
+                sg.popup("No item was selected. \n\nPlease select an item from the list.", title = "Item selection error")
+            # print(vals2["ItemList"])
+            else: 
+                item_index = vals2["ItemList"][0][0] - 1
+                item_from_Recipt = reciept.items[item_index]
+
+                winItem_active = True
+                colEdit = [
+                    [sg.Text("Welcome to the item creator!")],
+                    [sg.Text("Name of item"), sg.Input(key = "nameEd", default_text = item_from_Recipt.name)],
+                    [sg.Text("Price of item"), sg.Input(key = "priceEd", default_text = item_from_Recipt.price)],
+                    [sg.Text("Discount on item"), sg.Input(key = "discountEd", default_text = item_from_Recipt.discount)]
+                ]
+
+                defaultCheck = []
+                for option in showContri:
+                    append = False
+                    for name in item_from_Recipt.contributers:
+                        if option == name:
+                            append = not append
+                    defaultCheck.append(append)
+                    append = False
+
+                checkboxes = [sg.Checkbox(x, default= defaultCheck[k]) for k,x in enumerate(showContri)]
+                newContri = []
+                layoutItemEditor = [
+                    [sg.Column(colEdit)],
+                    [checkboxes, sg.Text(key = "-OUTPUT3-")],
+                    [sg.Button("Update"), sg.Button("Cancel")]
+                ]
+                winItem = sg.Window("Item editor", layoutItemEditor)
+
+            
+
+
+
         if winItem_active:
             ev3, vals3 = winItem.read(timeout = 100)
-            if ev3 == sg.WIN_CLOSED or ev3 == 'Exit':
+
+            if ev3 == sg.WIN_CLOSED or ev3 == "Cancel":
                 winItem_active  = False
                 winItem.close()
-            
-            if ev3 == "Add":                  
-                for i, contrib in enumerate(showContri):
-                    if checkboxes[i].Get():
-                        if contrib not in newContri:
-                            newContri.append(contrib)
-
 
                 winItem["-OUTPUT3-"].update(f"List of contributers for this item: {newContri}")
             
             if ev3 == "Finish":
-                func.AppendItem(vals3["name"], vals3["price"],vals3["discount"], newContri, relavantFile)
+
+                if func.IsFloat(vals3["price"]) and func.IsFloat(vals3["discount"]):
+                    newContri = []
+
+                    for j, contrib in enumerate(showContri):
+                        if checkboxes[j].Get():
+                            newContri.append(contrib)
+
+                    reciept.items.append(func.Item(vals3["name"], vals3["price"],vals3["discount"], newContri))
+                    
+                    priceTotal = float(vals3["price"]) - float(vals3["discount"])
+                    sharedPrice = priceTotal/len(newContri)
+                    subtotal += priceTotal
+                    for contrib in newContri:
+                        reciept.contributersToPay[contrib] += sharedPrice
+
+                    i += 1
+
+                    sg.popup("Item sucessfully added, closing item creator", auto_close=True, auto_close_duration=0.3, button_type = 5)
+
+                    itemList.append([i,vals3["name"], vals3["price"] ,vals3["discount"], newContri])
+
+                    win2["ItemList"].update(values = itemList)
+                    win2["subtotal"].update(subtotal)
+
+                    winItem_active  = False
+                    winItem.close()
+                else:
+                    sg.popup_error("ERROR \nLooks like something that was supposed to be a number wasn't \nPlease check that the fields price and discunt does not have any of the following: \n\tLetters \n\tCommas, use . instead \n\tOther symbols or signs")
+
+            
+            if ev3 == "Update":
+                newContri = []
+
+                for i, contrib in enumerate(showContri):
+                    if checkboxes[i].Get():
+                        newContri.append(contrib)
+
+                oldPriceTotal = float(reciept.items[item_index].price) - float(reciept.items[item_index].discount)
+                newPrice = float(vals3["priceEd"]) - float(vals3["discountEd"])
+                priceDiff = newPrice - oldPriceTotal
+
+
+
+                reciept.items[item_index] = (func.Item(vals3["nameEd"], vals3["priceEd"],vals3["discountEd"], newContri))
+
+                
+                sharedPriceDiff = priceDiff/len(newContri)
+                subtotal += priceDiff
+                for contrib in newContri:
+                    reciept.contributersToPay[contrib] += sharedPrice
+
+                sg.popup("Item sucessfully changed, closing item creator", auto_close=True, auto_close_duration=0.3, button_type = 5)
+
+                itemList[item_index] = [item_index + 1,vals3["nameEd"], vals3["priceEd"] ,vals3["discountEd"], newContri]
+
+                win2["ItemList"].update(values = itemList)
+
                 winItem_active  = False
                 winItem.close()
--- func.py
+++ func.py
@@ -1,12 +1,19 @@
 
-import json
+# import json
+nums = [1,2,3]
+
+nums_squared = [num**2 for num in nums]
+
 
 class Basics:
-    def __init__(self, date, store, payer, contributers) -> None:
+    def __init__(self, date, store, paidBy, contributers):
         self.date = date
         self.store = store
-        self.payer = payer
-        self.contributers = contributers
+        self.paidBy = paidBy
+        self.subtotal = 0
+        self.contributersToPay = []
+        for contributer in contributers:
+            self.contributersToPay.append({contributer : 0})
         self.items = []
 
 class Item:
@@ -16,29 +23,36 @@ class Item:
         self.discount = discount
         self.contributers = contributers
 
-def CreateFile(date, store, payer, contributers):
-    f = open(f"Receipt\{str(date)}_{store}.json", "x")
-    basics = Basics(date, store, payer, contributers).__dict__
-    f.write(json.dumps(basics))
-    f.close()
-    return f"Receipt\{str(date)}_{store}.json"
 
-def AppendItem(name, price, discount, contributers, relavantFile):
-    def write_json(new_data, filename= relavantFile):
-        with open(filename,'r+') as file:
-          # First we load existing data into a dict.
-            file_data = json.load(file)
-        # Join new_data with file_data inside emp_details
-            file_data["items"].append(new_data)
-        # Sets file's current position at offset.
-            file.seek(0)
-        # convert back to json.
-            json.dump(file_data, file)
+def IsFloat(value):
+    try:
+        float(value)
+        return True
+    except Exception:
+        return False
+
+# def CreateFile(date, store, payer):
+#     f = open(f"Receipt\{str(date)}_{store}.json", "x")
+#     basics = Basics(date, store, payer).__dict__
+#     f.write(json.dumps(basics))
+#     f.close()
+#     return f"Receipt\{str(date)}_{store}.json"
+
+# def AppendItem(name, price, discount, contributers, relavantFile):
+#     def write_json(new_data, filename= relavantFile):
+#         with open(filename,'r+') as file:
+#           # First we load existing data into a dict.
+#             file_data = json.load(file)
+#         # Join new_data with file_data inside emp_details
+#             file_data["items"].append(new_data)
+#         # Sets file's current position at offset.
+#             file.seek(0)
+#         # convert back to json.
+#             json.dump(file_data, file)
  
-    # python object to be appended
-    y = Item(name, price, discount, contributers).__dict__
+#     # python object to be appended
+#     y = Item(name, price, discount, contributers).__dict__
      
-    write_json(y)
-
+#     write_json(y)
 
 # dict = json.loads(f.read())
